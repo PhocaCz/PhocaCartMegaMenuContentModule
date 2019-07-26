e2938436476fd639ed1404923651bcaab1376c75
Jan <info@phoca.cz>
2019-07-26 16:11:01 +0200

    Initial commit

---
+++ mod_phocacart_mega_menu_content.php
@@ -0,0 +1,78 @@
+<?php
+/* @package Joomla
+ * @copyright Copyright (C) Open Source Matters. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * @extension Phoca Extension
+ * @copyright Copyright (C) Jan Pavelka www.phoca.cz
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ */
+
+defined('_JEXEC') or die;// no direct access
+
+if (!JComponentHelper::isEnabled('com_phocacart', true)) {
+	$app = JFactory::getApplication();
+	$app->enqueueMessage(JText::_('Phoca Cart Error'), JText::_('Phoca Cart is not installed on your system'), 'error');
+	return;
+}
+
+$document	= JFactory::getDocument();
+
+JLoader::registerPrefix('Phocacart', JPATH_ADMINISTRATOR . '/components/com_phocacart/libraries/phocacart');
+
+$lang = JFactory::getLanguage();
+//$lang->load('com_phocacart.sys');
+$lang->load('com_phocacart');
+
+$media = new PhocacartRenderMedia();
+$media->loadBase();
+$media->loadBootstrap();
+$media->loadSpec();
+JHtml::stylesheet('media/mod_phocacart_mega_menu_content/css/style.css');
+$s = PhocacartRenderStyle::getStyles();
+
+$pCom						= JComponentHelper::getParams( 'com_phocacart' );
+$pc['display_webp_images']	= $pCom->get( 'display_webp_images', 0 );
+
+
+$p['type']					= $params->get( 'type', 1 );
+$p['ordering']				= $params->get( 'ordering', 1 );
+$p['columns']				= $params->get( 'columns', 3 );
+$p['items']					= $params->get( 'items', 3 );
+$p['display_title']			= $params->get( 'display_title', 1 );
+$p['display_link']			= $params->get( 'display_link', 1 );
+$p['display_image']			= $params->get( 'display_image', 1 );
+$p['display_desc']			= $params->get( 'display_desc', 1 );
+$p['display_subcategories']	= $params->get( 'display_subcategories', 1 );
+$moduleclass_sfx 			= htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8');
+
+
+$categories		= array();
+$manufacturers	= array();
+
+$filter_language	= $params->get( 'filter_language', 0 );
+	$language = '';
+	if ($filter_language == 1) {
+		//$lang 		= JFactory::getLanguage();
+		$language	= $lang->getTag();
+	}
+
+if ($p['type'] == 1) {
+	$display_categories = $params->get('display_categories', '');
+	$hide_categories 	= $params->get('hide_categories', '');
+
+	if (!empty($display_categories)) {
+		$display_categories = implode(',', $display_categories);
+	}
+	if (!empty($hide_categories)) {
+		$hide_categories = implode(',', $hide_categories);
+	}
+
+	$categories 		= PhocacartCategory::getCategoryTreeArray($p['ordering'], $display_categories, $hide_categories, array(0 ,1), $language);
+
+} else if ($p['type'] == 2) {
+
+	$manufacturers = PhocacartManufacturer::getAllManufacturers($p['ordering'], 0, $language);
+}
+
+require(JModuleHelper::getLayoutPath('mod_phocacart_mega_menu_content'));
+?>
---
+++ tmpl/default.php
@@ -0,0 +1,169 @@
+<?php
+/* @package Joomla
+ * @copyright Copyright (C) Open Source Matters. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * @extension Phoca Extension
+ * @copyright Copyright (C) Jan Pavelka www.phoca.cz
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ */
+defined('_JEXEC') or die;
+
+$layoutI	= new JLayoutFile('image', null, array('component' => 'com_phocacart'));
+$col 		= PhocacartRenderFront::getColumnClass((int)$p['columns']);
+
+echo '<div class="ph-mega-menu-content-module-box'.$moduleclass_sfx .'">';
+
+
+
+
+if ($p['type'] == 1 && !empty($categories)) {
+
+    // CATEGORIES
+    $class = 'ph-cat';
+
+    echo '<div class="'.$s['c']['row'].' ph-mega-menu-content-row '.$class.'">';
+    $i = 0;
+    foreach ($categories as $k => $v) {
+
+        if ($i >= $p['items']) {
+            break;
+        }
+
+        if ((int)$v['id'] > 0 && $v['title'] != '' && $v['alias'] != '') {
+
+            echo '<div class="'.$s['c']['row-item'].' '.$s['c']["col.xs12.sm{$col}.md{$col}"].' ph-mega-menu-content-row-item '.$class.'">';
+
+            $link = PhocacartRoute::getCategoryRoute($v['id'], $v['alias']);
+
+            if ($p['display_title'] == 1) {
+
+                echo '<h3 class="ph-mega-menu-content-header '.$class.'">';
+                echo $p['display_link'] == 1 ? '<a href="' . $link . '">' : '';
+                echo $v['title'];
+                echo $p['display_link'] == 1 ? '</a>' : '';
+                echo '</h3>';
+            }
+
+
+
+            if ($p['display_image'] == 1 && $v['image'] != '') {
+
+                $image = PhocacartImage::getThumbnailName(PhocacartPath::getPath('categoryimage'), $v['image'], 'small');
+                $d = array();
+                $d['s'] = $s;
+                $d['t'] = $pc;
+                $d['src'] = JURI::base(true) . '/' . $image->rel;
+                $d['srcset-webp'] = JURI::base(true) . '/' . $image->rel_webp;
+                $d['alt-value'] = PhocaCartImage::getAltTitle($v['title'], $image->rel);
+                $d['class'] = $s['c']['img-responsive'] . ' ph-image ph-mega-menu-content-image '.$class;
+
+                echo $p['display_link'] == 1 ? '<a href="' . $link . '">' : '';
+                echo $layoutI->render($d);
+                echo $p['display_link'] == 1 ? '</a>' : '';
+            }
+
+            if ($p['display_desc'] == 1 && $v['description'] != '') {
+                echo '<div class="ph-mega-menu-content-description '.$class.'">';
+                echo $v['description'];
+                echo '</div>';
+
+            }
+
+            if ($p['display_subcategories'] == 1 && !empty($v['children'])) {
+                echo '<ul class="ph-mega-menu-content-subcategories '.$class.'">';
+                foreach ($v['children'] as $k2 => $v2) {
+
+                    if ((int)$v2['id'] > 0 && $v2['title'] != '' && $v2['alias'] != '') {
+                        $link = PhocacartRoute::getCategoryRoute($v2['id'], $v2['alias']);
+                        echo '<li>';
+                        echo '<a href="' . $link . '">';
+                        echo $v2['title'];
+                        echo '</a>';
+                        echo '</li>';
+                    }
+
+                }
+                echo '</ul>';
+            }
+
+            echo '</div>';// end row item
+        }
+
+        $i++;
+
+    }
+
+    echo '</div>';// end row
+
+
+} else if ($p['type'] == 2 && !empty($manufacturers)) {
+
+    // MANUFACTURERES
+    $class = 'ph-man';
+
+    echo '<div class="'.$s['c']['row'].' ph-mega-menu-content-row '.$class.'">';
+    $i = 0;
+    foreach ($manufacturers as $k => $v) {
+
+        if ($i >= $p['items']) {
+            break;
+        }
+
+        if ((int)$v->id > 0 && $v->title != '' && $v->alias != '') {
+
+            echo '<div class="'.$s['c']['row-item'].' '.$s['c']["col.xs12.sm{$col}.md{$col}"].' ph-mega-menu-content-row-item '.$class.'">';
+
+            $link = PhocacartRoute::getItemsRoute('', '', 'manufacturer', $v->id . '-'.$v->alias);
+
+            if ($p['display_title'] == 1) {
+
+                echo '<h3 class="ph-mega-menu-content-header '.$class.'">';
+                echo $p['display_link'] == 1 ? '<a href="' . $link . '">' : '';
+                echo $v->title;
+                echo $p['display_link'] == 1 ? '</a>' : '';
+                echo '</h3>';
+            }
+
+
+
+            if ($p['display_image'] == 1 && $v->image != '') {
+
+
+                $d = array();
+                $d['s'] = $s;
+                $d['t'] = $pc;
+                $d['src'] = JURI::base(true) . '/' . $v->image;
+                $d['srcset-webp'] = '';//JURI::base(true) . '/' . $image->rel_webp;
+                $d['alt-value'] = PhocaCartImage::getAltTitle($v->title, $v->image);
+                $d['class'] = $s['c']['img-responsive'] . ' ph-image ph-mega-menu-content-image '.$class;
+
+                echo $p['display_link'] == 1 ? '<a href="' . $link . '">' : '';
+                echo $layoutI->render($d);
+                echo $p['display_link'] == 1 ? '</a>' : '';
+            }
+
+            if ($p['display_desc'] == 1 && $v->description != '') {
+                echo '<div class="ph-mega-menu-content-description '.$class.'">';
+                echo $v->description;
+                echo '</div>';
+
+            }
+
+            echo '</div>';// end row item
+        }
+
+        $i++;
+
+    }
+}
+
+
+
+
+
+
+
+echo '</div>';
+?>
+
+
